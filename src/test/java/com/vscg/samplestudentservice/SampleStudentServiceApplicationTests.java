9a743d7f022462db1a31a9c4275989535a8e6f63
Sergio Cruz <vlad.crz@gmail.com>
2023-12-11 21:58:32 -0600

    Initial commit

---
+++ src/test/java/com/vscg/samplestudentservice/SampleStudentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vscg.samplestudentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleStudentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
