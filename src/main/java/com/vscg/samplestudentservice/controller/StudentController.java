47b151ab56876142c33f10ecf8397b0545e55aa7
Sergio Cruz <vlad.crz@gmail.com>
2023-12-13 22:00:26 -0600

    Minor changes in path

--- src/main/java/com/vscg/samplestudentservice/controller/StudentController.java
+++ src/main/java/com/vscg/samplestudentservice/controller/StudentController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RequestMapping(path = "/students")
+@RequestMapping(path = "/v1/students")
 @RestController
 public class StudentController {
 
